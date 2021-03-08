2a3dd7042944334219404d6d5fef141093d2b73c
mafaldamurador <mafaldamurador@localhost>
2021-03-08 11:21:32 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -15,6 +15,44 @@ public class Libretto {
 		this.voti.add(v);		
 	}
 	
+	/*
+	public void stampaVotiUguali(int punteggio) {
+		//il libretto stampa da solo i voti
+		System.out.println();
+	}
+	
+	public String votiUguali(int punteggio) {
+		//calcola una stringa che contiene i voti
+		//sarà poi il chiamante a stamparli
+		//--> solo il nome?
+		//--> Tutto il voto.toString()
+	}
+	*/
+	
+	public List<Voto> listaVotiUguali(int punteggio){
+		//restituisce solo i voti uguali al criterio
+		List<Voto> risultato = new ArrayList<>();
+		for(Voto v: this.voti) {
+			if(v.getVoto()==punteggio) {
+				risultato.add(v);
+			}
+		}
+		return risultato;
+	}
+	
+	
+	public Libretto votiUguali(int punteggio) {
+		Libretto risultato=new Libretto();
+		for(Voto v: this.voti) {
+			if(v.getVoto()==punteggio) {
+				risultato.add(v);
+			}
+		}
+		return risultato; 
+	}
+	
+	
+	
 	public String toString() {
 		String s= "";
 		for(Voto v: this.voti) {
@@ -22,6 +60,4 @@ public class Libretto {
 		}
 		return s;
 	}
-	
-
 }
